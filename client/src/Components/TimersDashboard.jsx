d2ad27a3510118052d2b20d3b3c900f84db1121b
Temitope <tope_olajide@outlook.com>
2019-02-23 16:50:57 +0100

    Create TimersDashboard.jsx

---
+++ client/src/Components/TimersDashboard.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import { Container, Row, Col } from "react-bootstrap";
+class TimersDashboard extends Component {
+     render() {
+          return (
+            <Container>
+  <Row>
+    
+  </Row>
+  </Container>
+          )
+        }
+    }
+export default TimersDashboard;
