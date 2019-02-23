5a78dbef96d2320eb830601418e92f5bd2309f73
Temitope <tope_olajide@outlook.com>
2019-02-23 19:29:39 +0100

    Update TimersDashboard.jsx

--- client/src/Components/TimersDashboard.jsx
+++ client/src/Components/TimersDashboard.jsx
@@ -1,14 +1,19 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import EditableTimerList from "./EditableTimerList";
+import ToggleableTimerForm from "./ToggleableTimerForm";
 import { Container, Row, Col } from "react-bootstrap";
 class TimersDashboard extends Component {
-     render() {
-          return (
-            <Container>
-  <Row>
-    
-  </Row>
-  </Container>
-          )
-        }
-    }
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <EditableTimerList />
+            <ToggleableTimerForm isOpen={true} />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
 export default TimersDashboard;
