56b54c17a85d1f845a542b6b53c6468078d34d9a
Temitope <tope_olajide@outlook.com>
2019-02-23 17:16:20 +0100

    Update TimerForm.jsx

--- client/src/Components/TimerForm.jsx
+++ client/src/Components/TimerForm.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Card, Form, Button, Col } from "react-bootstrap";
 class TimerForm extends Component {
   render() {
+    const submitText = this.props.title ? 'Update' : 'Create';
     return (
       <Col sm={3}>
         <Card>
@@ -12,16 +13,16 @@ class TimerForm extends Component {
                 <Card.Title className="text-left">
                   <Form.Label>Title </Form.Label>
                 </Card.Title>
-                <Form.Control type="text" />
+                <Form.Control type="text" defaultValue={this.props.title} />
               </Form.Group>
               <Form.Group>
                 <Card.Title className="text-left">Project</Card.Title>
-                <Form.Control type="text" />
+                <Form.Control type="text" defaultValue={this.props.project}/>
               </Form.Group>
             </Form>
           </Card.Body>
           <Card.Footer>
-            <Button variant="light">Update</Button>
+            <Button variant="light">{submitText}</Button>
             <Button variant="light">Cancel</Button>
           </Card.Footer>
         </Card>
