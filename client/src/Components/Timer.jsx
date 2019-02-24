5f491dcb17e3a5b91749edcf7ff5b36a2aa4dc2c
Temitope <tope_olajide@outlook.com>
2019-02-24 23:54:57 +0100

    Update Timer.jsx

--- client/src/Components/Timer.jsx
+++ client/src/Components/Timer.jsx
@@ -1,28 +1,28 @@
 import React, { Component } from "react";
-import { Card, Button, Col } from "react-bootstrap";
+import { Card, Button,Col } from "react-bootstrap";
 class Timer extends Component {
   render() {
+    
     return (
-      <Col sm={3}>
-        <Card>
-          <Card.Header>Timer</Card.Header>
-          <Card.Body>
-            <Card.Title className="text-left">
-              Special title treatment
-            </Card.Title>
-            <Card.Subtitle className="mb-2 text-muted text-left">
-              With supporting
-            </Card.Subtitle>
-            <h1>01:30:56</h1>
-          </Card.Body>
-          <Card.Footer>
-            {" "}
-            <Button variant="light">Start</Button>
-            <Button variant="light">Edit</Button>
-            <Button variant="light">Delete</Button>
-          </Card.Footer>
-        </Card>{" "}
-      </Col>
+      <Col md={4}>
+      <Card>
+      <Card.Header></Card.Header>
+              <Card.Body>
+                <Card.Title className="text-left">
+                  <h3>{this.props.projectTitle}</h3>
+                </Card.Title>
+                <Card.Subtitle className="mb-2 text-muted text-left">
+                 { this.props.projectDescription}
+                </Card.Subtitle>
+                <h1>{ this.props.hours}:{ this.props.minutes}:{ this.props.seconds}</h1>
+              </Card.Body>
+              <Card.Footer>
+                <Button variant="light">Start</Button>
+                <Button variant="light">Edit</Button>
+                <Button variant="light">Delete</Button>
+              </Card.Footer>
+              </Card>{" "}
+          </Col>
     );
   }
 }
